ae988b10f1c9084ecae8c55ec31d5f9f55cc9f4a
dungnguyen <dungna.qn@gmail.com>
2024-07-29 17:07:34 +0700

    initial project

---
+++ src/main/java/org/example/casestudymodule4shoestore/CaseStudyModule4ShoeStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.casestudymodule4shoestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyModule4ShoeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyModule4ShoeStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/casestudymodule4shoestore/CaseStudyModule4ShoeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.casestudymodule4shoestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyModule4ShoeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
