33d4ab0236f992f8184a0ce852e99501a95e18ff
dungnguyen <dungna.qn@gmail.com>
2024-07-30 08:14:12 +0700

    adjust views

---
+++ src/main/java/org/example/casestudymodule4shoestore/controllers/UserController.java
@@ -0,0 +1,45 @@
+package org.example.casestudymodule4shoestore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+    @GetMapping
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/shop")
+    public String shop(){
+        return "shop";
+    }
+
+    @GetMapping("/cart")
+    public String cart(){
+        return "cart";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout(){
+        return "checkout";
+    }
+
+    @GetMapping("/thankyou")
+    public String thankYou(){
+        return "thankyou";
+    }
+
+}
