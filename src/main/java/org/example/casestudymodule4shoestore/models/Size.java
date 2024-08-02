44cbec57d60b3a22a49a73f23bb1772fc906f159
GohanVu <hoangvhdata@gmail.com>
2024-08-02 15:33:26 +0700

    update navabar + fix  permitall

--- src/main/java/org/example/casestudymodule4shoestore/models/Size.java
+++ src/main/java/org/example/casestudymodule4shoestore/models/Size.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "size", schema = "module4_casestudy_shoe_store")
-public class Size {
+public class  Size {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
