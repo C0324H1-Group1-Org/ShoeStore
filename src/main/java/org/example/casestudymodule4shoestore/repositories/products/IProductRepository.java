c2cd953305f7286fac616af23d0f1170a11bfe7a
Seitama179 <ngochd997@gmail.com>
2024-08-07 14:17:02 +0700

    ngoc commit

--- src/main/java/org/example/casestudymodule4shoestore/repositories/products/IProductRepository.java
+++ src/main/java/org/example/casestudymodule4shoestore/repositories/products/IProductRepository.java
@@ -13,9 +13,6 @@ public interface IProductRepository extends JpaRepository<Product, Long> {
     @Query( nativeQuery = true, value = "select p.*  from products as p order by p.price desc")
     List<Product> sortProductsByPrice();
 
-//    @Query( nativeQuery = true, value = "select p.id as id, p.name as name, p.image as image, p.price as price, c.id as categoryId, c.name as categoryName from products p join categories c on p.cat_id = c.id where c.id = :id")
-//    List<ProductCategoryDTO> findByCategory(@Param("id") Long id);
-
     List<Product> findAllByCat_Id(Integer id);
 
     @Query(nativeQuery = true, value = "select p.*  from products as p where p.name like :keyword")
